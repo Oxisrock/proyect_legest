9d84937b63649c86e70c705525eec22ab598307b
oxisrock <franciscoaular18@gmail.com>
2017-05-22 18:20:20 -0430

    Creando donde estaran las configuraciones de coneccion

---
+++ Backend/config.js
@@ -0,0 +1,16 @@
+'use strict'
+// donde se iniciara el servidor
+const mongoose = require('mongoose')// se hace require de el modulo de mongoose
+const app = require('./app') // se hace require de app.js que es donde estan las configuraciones de express
+const config = require('./config') // se hace require de config.js es donde estan la url de la db y el puerto
+
+mongoose.connect(config.db, (err, res) => { // llama el metodo connect de mongoose
+  if (err) { // si ocurre un Error
+    return console.log(`Error al conectar a la base de datos ${err}`) // manda a imprimir en consola
+  }
+  console.log('Conexion a la base de datos establecida')// manda a imprimir que no hubo error
+// manda a escuchar al servidor de express
+  app.listen(config.port, function () { // el metodo app.listen de express es el que levanta el servidor
+    console.log('API REST corriendo en http://localhost :' + config.port) // se manda a imprimir para verificar si el server levanto
+  })
+})
