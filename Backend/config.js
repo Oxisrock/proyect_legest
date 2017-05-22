9e49679f1b1effc497c0feac19df10c8113f2371
oxisrock <franciscoaular18@gmail.com>
2017-05-22 18:31:15 -0430

    Configuracion de local puerto y coneccion a la base de datos

--- Backend/config.js
+++ Backend/config.js
@@ -1,16 +1,7 @@
-'use strict'
-// donde se iniciara el servidor
-const mongoose = require('mongoose')// se hace require de el modulo de mongoose
-const app = require('./app') // se hace require de app.js que es donde estan las configuraciones de express
-const config = require('./config') // se hace require de config.js es donde estan la url de la db y el puerto
-
-mongoose.connect(config.db, (err, res) => { // llama el metodo connect de mongoose
-  if (err) { // si ocurre un Error
-    return console.log(`Error al conectar a la base de datos ${err}`) // manda a imprimir en consola
-  }
-  console.log('Conexion a la base de datos establecida')// manda a imprimir que no hubo error
-// manda a escuchar al servidor de express
-  app.listen(config.port, function () { // el metodo app.listen de express es el que levanta el servidor
-    console.log('API REST corriendo en http://localhost :' + config.port) // se manda a imprimir para verificar si el server levanto
-  })
-})
+// donde se guardan las configuraciones de las conexion a la base de datos y el puerto donde se iniciara
+module.exports =
+{
+  port: process.env.PORT || 3000, // constante port para guardar el puerto donde se levantara el servidor
+  db: process.env.MONGODB || 'mongodb://127.0.0.1:27017/friender_db' // constante db es para guardar la ruta de la base de datos
+  // SECRET_TOKEN: 'miclavedetokens'
+}
