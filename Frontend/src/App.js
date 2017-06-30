18b8c32e929a83826aa0685c603fd3c35c6733f9
oxisrock <franciscoaular18@gmail.com>
2017-06-30 18:46:47 -0430

    Ya haciendo llamado desde el frontend al api con axio y redux

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,6 +1,50 @@
 import React, { Component } from 'react'
 import Layout from './pages/Layout'
 import './App.css'
+import { applyMiddleware, createStore } from 'redux'
+import axios from 'axios'
+import logger from 'redux-logger'
+import thunk from 'redux-thunk'
+const initialState = {
+  fetching: false,
+  fetched: false,
+  users: [],
+  error: null
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'FETCH_USERS_START':
+      {
+        return {...state, fetching: true}
+        break
+      }
+    case 'FETCH_USERS_ERROR':
+      {
+        return {...state, fetching: false, error: action.payload}
+        break
+      }
+    case 'RECEIVE_USERS':
+      {
+        return {...state, fetching: false, fetched: true, users: action.payload
+        }
+        break
+      }
+  }
+  return state
+}
+const middleware = applyMiddleware(thunk, logger)
+const store = createStore(reducer, middleware)
+store.dispatch((dispatch) => {
+  dispatch({type: 'FETCH_USERS_START'})
+  axios.get('http://127.0.0.1:1818/users')
+  .then((response) => {
+    dispatch({type: 'RECEIVE_USERS', payload: response.data})
+  })
+  .catch((err) => {
+    dispatch({type: 'FETCH_USERS_ERROR', payload: err})
+  })
+})
 class App extends Component {
   render () {
     const {location} = this.props
--- Frontend/src/pages/Location.js
+++ Frontend/src/pages/Location.js
@@ -2,23 +2,10 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { showUsers } from '../actions'
 class Location extends Component {
-  renderUsersList () {
-    return this.props.users.map((user) => {
-      return (
-        <li key={user.id}>{user.name}</li>
-      )
-    })
-  }
-  componentWillMount () {
-    this.props.showUsers()
-  }
   render () {
     return (
       <div className=' container jumbotron'>
         <h2 className='text-center text-shadow '> Users List </h2>
-        <ul>
-          { this.renderUsersList()}
-        </ul>
       </div>
     )
   }
